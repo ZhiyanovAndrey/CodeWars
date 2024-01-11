9aaa6ecd86967f5b5d7577a25580bdb5cd0cbb88
Андрей Жиянов <aazhiyanov@gmail.com>
2024-01-11 09:08:47 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+
+Console.ReadKey();
+
+public class StockList
+{
+
+    public static string stockSummary(String[] lstOfArt, String[] lstOf1stLetter)
+    {
+        return string.Empty;
+    }
+
+
+
+    //[Test]
+    //public void Test1()
+    //{
+    //    string[] art = new string[] { "ABAR 200", "CDXE 500", "BKWR 250", "BTSQ 890", "DRTY 600" };
+    //    String[] cd = new String[] { "A", "B" };
+    //    Assert.AreEqual("(A : 200) - (B : 1140)", StockList.stockSummary(art, cd));
+    //}
+}
