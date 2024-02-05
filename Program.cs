4e07a304da61cec50e90c2307f18d1ebb9d59889
Андрей Жиянов <aazhiyanov@gmail.com>
2024-02-05 14:14:09 +0300

    RomanConvert is done

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ using System.Text;
 
 
 
-Console.WriteLine(RomanConvert.Solution(188)); // CLII
+Console.WriteLine(RomanConvert.Solution(2014)); // MMXIV
 
 Console.ReadKey();
 
@@ -39,59 +39,64 @@ public class RomanConvert
                 Console.WriteLine(item);
             }
             //  необходимо сначала записать число тысяч, затем сотен, затем десятков и, наконец, единиц. 
-            if (stack.Count == 3) // извлекаем верхний разряд
+            if (stack.Count == 4) // извлекаем 1000
+            {
+                switch (stack.Pop())
+                {
+                    case 1: sb.Append("M"); break;
+                    case 2: sb.Append("MM"); break;
+                    case 3: sb.Append("MMM"); break;
+                   
+                }
+            }
+
+            if (stack.Count == 3) // извлекаем 100
             {
                 switch (stack.Pop())
                 {
                     case 1: sb.Append("C"); break;
                     case 2: sb.Append("CC"); break;
-                    case 3: sb.Append( "CCC"); break;
-                    case 4: sb.Append( "CD"); break;
-                    case 5: sb.Append( "D"); break;
-                    case 6: sb.Append( "DC"); break;
-                    case 7: sb.Append( "DCC"); break;
-                    case 8: sb.Append( "DCCC"); break;
-                    case 9: sb.Append( "CM"); break;
+                    case 3: sb.Append("CCC"); break;
+                    case 4: sb.Append("CD"); break;
+                    case 5: sb.Append("D"); break;
+                    case 6: sb.Append("DC"); break;
+                    case 7: sb.Append("DCC"); break;
+                    case 8: sb.Append("DCCC"); break;
+                    case 9: sb.Append("CM"); break;
                 }
-               }
-
-            if (stack.Count == 2) 
-            {
-                switch (stack.Pop())
-                {
-                    //case 1: rezult = "X"; break;
-                    case 2: sb.Append( "XX"); break;
-                    case 3: sb.Append( "XXX"); break;
-                    case 4: sb.Append( "XL"); break;
-                    case 5: sb.Append( "L"); break;
-                    case 6: sb.Append( "LX"); break;
-                    case 7: sb.Append( "LXX"); break;
-                    case 8: sb.Append( "LXXX"); break;
-                    case 9: sb.Append( "XC"); break;
-                }
-              
             }
 
-            if (stack.Count == 1 )
+            if (stack.Count == 2) // извлекаем 10
             {
                 switch (stack.Pop())
                 {
-                    case 1: sb.Append( "I"); break;
-                    case 2: sb.Append( "II"); break;
-                    case 3: sb.Append( "III"); break;
-                    case 4: sb.Append( "IV"); break;
-                    case 5: sb.Append( "V"); break;
-                    case 6: sb.Append( "VI"); break;
-                    case 7: sb.Append( "VII"); break;
-                    case 8: sb.Append( "VIII"); break;
-                    case 9: sb.Append( "IX"); break;
-                    //case 10: rezult = "X"; break;
-                    //case 11: rezult = "XI"; break;
-                    //case 12: rezult = "XII"; break;
-                    //case 13: rezult = "XII"; break;
-                    //case 14: rezult = "XIV"; break;
-                    //case 15: rezult = "XV"; break;
-                    //case 16: rezult = "IX"; break;
+                    case 1: sb.Append("X"); break;
+                    case 2: sb.Append("XX"); break;
+                    case 3: sb.Append("XXX"); break;
+                    case 4: sb.Append("XL"); break;
+                    case 5: sb.Append("L"); break;
+                    case 6: sb.Append("LX"); break;
+                    case 7: sb.Append("LXX"); break;
+                    case 8: sb.Append("LXXX"); break;
+                    case 9: sb.Append("XC"); break;
+                }
+
+            }
+
+            if (stack.Count == 1)
+            {
+                switch (stack.Pop())
+                {
+                    case 1: sb.Append("I"); break;
+                    case 2: sb.Append("II"); break;
+                    case 3: sb.Append("III"); break;
+                    case 4: sb.Append("IV"); break;
+                    case 5: sb.Append("V"); break;
+                    case 6: sb.Append("VI"); break;
+                    case 7: sb.Append("VII"); break;
+                    case 8: sb.Append("VIII"); break;
+                    case 9: sb.Append("IX"); break;
+
                 }
             }
             return sb.ToString();
@@ -103,7 +108,8 @@ public class RomanConvert
         return "Please, enter integer between 1 and 3999";
     }
 }
-
+// При этом некоторые из цифр (I, X, C, M) могут повторяться, но не более трёх раз подряд; таким образом,
+// с их помощью можно записать любое натуральное число не более 3999 (MMMCMXCIX).
 //I          1
 //V          5
 //X          10
